6ef585d1c8e0f5a52e144b9d1304f42b71ea47af
ujangaja <ujangaja@gmail.com>
2023-02-11 14:18:35 +0700

    Installasi FontAwesom & Framwork Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faCircleCheck,
+  faPen,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons"
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container App">
+      <br />
+      <br />
+      <h2>To Do List App ( ReactJS)</h2>
+      <br />
+      <br />
+      WebSite React App
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
