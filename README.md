d5bb06e85d84a1959f2d8ea3901c44c118611022
ujangaja <ujangaja@gmail.com>
2023-02-11 14:58:12 +0700

    Logic & Styling list todo

--- README.md
+++ README.md
@@ -12,3 +12,16 @@
             - npm install --save bootstrap
         3.  App.js
         4.  App.css
+
+## Logic & Styling list todo
+
+    Todo:
+
+        1.  App.js
+            - Tasks (ToDO List) State
+            - Temp state
+            - All function in Todo
+            - Display ToDos
+            - Display ToDos
+        2.  App.css
+            styling css
--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
@@ -8,6 +9,38 @@ import {
 import "./App.css"
 
 function App() {
+  // Tasks (ToDO List) State
+
+  const [toDo, setToDo] = useState([
+    { id: 1, title: "Task 1", status: true },
+    { id: 2, title: "Task 2", status: false },
+  ])
+
+  // Temp state
+  const [newTask, setNewTask] = useState("")
+  const [updateData, setUpdateData] = useState("")
+
+  // All function in Todo
+
+  // add tasks
+
+  const addTask = () => {}
+
+  // Delete tasks
+  const deleteTask = (id) => {}
+
+  // Mark tasks as done or completed
+  const markDone = (id) => {}
+
+  // Cancel update
+  const cancelUpdate = () => {}
+
+  // Change taks for update
+  const changeTask = (e) => {}
+
+  // Update tasks
+  const updateTask = () => {}
+
   return (
     <div className="container App">
       <br />
@@ -15,7 +48,26 @@ function App() {
       <h2>To Do List App ( ReactJS)</h2>
       <br />
       <br />
-      WebSite React App
+      {/* Display ToDos */}
+      {/* logic jika todo tidak ada datanya */}
+      {toDo && toDo.length ? "" : "No Tasks..."}
+      {/* menampilkan data dari todo dengan map */}
+      {toDo &&
+        toDo.map((task, index) => {
+          return (
+            <React.Fragment key={task.id}>
+              <div className="col taskBg">
+                {/* kondisi jika status bernilai done , maka tulisan todo akan dicoret */}
+                <div className={task.status ? "done" : ""}>
+                  {/* nomro urut todo */}
+                  <span className="taskNumber">{index + 1}</span>
+                  {/* data todo */}
+                  <span className="taskText">{task.title}</span>
+                </div>
+              </div>
+            </React.Fragment>
+          )
+        })}
     </div>
   )
 }
